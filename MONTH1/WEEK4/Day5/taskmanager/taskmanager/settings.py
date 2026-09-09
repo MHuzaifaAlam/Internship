78966abc7d2439de9b9c583f32fe65e60c6bd504
M Huzaifa Alam <mhuzaifaalam7@gmail.com>
2026-09-09 10:38:02 +0500

    The middleware and the signals practice is done

--- MONTH1/WEEK4/Day5/taskmanager/taskmanager/settings.py
+++ MONTH1/WEEK4/Day5/taskmanager/taskmanager/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "tasks.middleware.RequestLoggingMiddleware"
 ]
 
 ROOT_URLCONF = 'taskmanager.urls'
