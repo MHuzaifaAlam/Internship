7e6fd4ae4d9ea3ca2e036fcacdc14ee9ae569b24
M Huzaifa Alam <mhuzaifaalam7@gmail.com>
2026-09-14 16:21:28 +0500

    Practicing Serializers

--- MONTH1/WEEK4/Day5/taskmanager/tasks/serializers.py
+++ MONTH1/WEEK4/Day5/taskmanager/tasks/serializers.py
@@ -1,9 +1,49 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task,Project,User
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+class ProjectSerializer(serializers.ModelSerializer):
+
+
+
+
+    class Meta:
+        model=Project
+        fields=["id","title"]
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=User
+        fields=["id","username"]
+
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    project=ProjectSerializer()
+    assigned_to=UserSerializer(many=True)
+    def validate_title(self,value):
+        if len(value)<5:
+            raise serializers.ValidationError("" \
+            "TITLE MUST BE AT LEAST 5 CHARACTERS LONG")
+        return value
 
+    def validate(self, data):
+        estimated=data.get("estimated_hours")
+        actual=data.get("actual_hours")
+
+        if actual>estimated:
+            raise serializers.ValidationError(
+                "Actual hours cannot be greater then estimated hours"
+            )
+        return data
+    def validate_estimated_hours(self,value):
+        if value <= 0:
+            raise serializers.ValidationError("The estimated hours can not be Zero or Less then Zero")
+
+        
     class Meta:
         model = Task
         fields = "__all__"
+
